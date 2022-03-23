7dd5fec50fe763dcdcf33aaab8cfa473456501ed
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-23 13:26:46 +0100

    pswgen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // alert('Hello World!');
+
+let userName = prompt('Inserisci il tuo nome');
+let userLastname = prompt('Inserisci il tuo cognome');
+let userColor = prompt('Inserisci il tuo colore preferito');
+let nGen = 21;
+
+let pswGen = userName + userLastname + userColor + nGen;
+
+document.getElementById('psw').innerHTML += ' ' + pswGen;
+
+console.log('Il tuo nome è:', userName);
+console.log('Il tuo cognome è:',userLastname);
+console.log('Il tuo colore preferito è:',userColor);
