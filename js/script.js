e513627c38fb832b3f837b640beecdfafc93899c
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-23 17:01:57 +0100

    aggiunto flowchart da Mermaid

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// flowchart TD
+//    A[INIZIO] --> B[Richiedi di inserie il nome]
+//    B --> C[memorizza il nome nella variabile 'userName']
+//    C --> D[Richiedi di inserire il cognome]
+//    D --> E[memorizza il cognome nella variabile 'userLastname'] 
+//    E --> F[Richiedi il di inserire il colore preferito]
+//    F --> G[memorizza il colore nella variabile 'userColor']
+//    G --> H[memorizza '21' nella variabile 'nGen']
+//    H --> I[in script.js, nella variabile 'pswGen' concatena le variabili 'userName' + 'userLastname' + 'userColor' + 'nGen']
+//    I --> L[stampa  index.html.#psw concatenata con pswGen]
+//    L --> M[stampa in console 'userName']
+//    M --> N[stampa in console 'userLastname']
+//    N --> O[stampa in console 'userColor']
+//    O --> P[FINE]
 // alert('Hello World!');
 
 let userName = prompt('Inserisci il tuo nome');
